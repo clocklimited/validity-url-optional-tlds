07edf668b1fc2369a6b349e174f382d5302e7e59
James Mortemore <james.mortemore@clock.co.uk>
2013-11-04 15:30:17 +0000

    Initial commit

---
+++ test/validity-url-optional-tlds.test.js
@@ -0,0 +1,76 @@
+var validate = require('../validator')()
+  , async = require('async')
+  , should = require('should')
+
+describe('URL Validation', function() {
+
+  it('should correctly validate valid URLs', function (done) {
+    var urls =
+    [ 'http://www.google.com'
+    , 'https://www.google.com'
+    ,  'http://google.com'
+    ,  'https://google.com'
+    ,  'http://asddasads.google.com'
+    ,  'http://asddasads.google.com/asddasads'
+    ,  'http://asddasads.google.com/asddasads/asdass'
+    ,  'http://asddasads.google.com/asddasads/asdass#'
+    ,  'http://asddasads.google.com/asddasads/asdass?a=b'
+    ,  'http://asddasads.google.com/asddasads/asdass?a=b#asdasd'
+    ,  'http://a.co'
+    ,  'http://user:password@a.com'
+    ,  'http://user:password@a.com?f=b&b=a'
+    ,  'http://user:password@www.a.com?f=b&b=a'
+    ,  'http://user:password@www.a.a.a.a.a.a.a.a.a.a.a.a.com/mypage.html?f=b&b=a#'
+    ,  'http://localhost'
+    ,  'http://localhost:80'
+    ,  'ftp://localhost:21'
+    ,  'http://localhost/test'
+    ,  'http://intranet:80'
+    ,  'http://intranet:80/test'
+    ]
+
+    async.each(urls, function (value, cb) {
+      validate('url', 'url', { url: value }, function (err, errMessage) {
+        if (err) return cb(err)
+        should.not.exist(errMessage)
+
+        cb(err)
+      })
+    }, function (err) {
+      done(err)
+    })
+
+  })
+
+  it('should correctly return false for invaild URLs', function (done) {
+    var urls = 
+    [ 'google.com'
+    , 'sfsdfsd'
+    , 'http://'
+    , 'www.fred.com'
+    , 'google.com/asddasads/asdass?a=b#asdasd'
+    , 'localhost'
+    , 'localhost:80'
+    ,  null
+    , undefined
+    , ''
+    ,  new Date()
+    ,  1
+    ,  '1'
+    ]
+
+    async.each(urls, function(value, cb) {
+      validate('url', 'url', { url: value }, function (err, errMessage) {
+        if (err) return cb(err)
+        
+        errMessage.should.equal('url must be a valid URL')
+
+        cb(err)
+      })
+    }, function (err) {
+      done(err)
+    })
+
+  })
+
+})
---
+++ validator.js
@@ -0,0 +1,29 @@
+module.exports = createValidator
+
+function createValidator() {
+
+  function validate(key, keyDisplayName, object, cb) {
+
+    if (!urlMatch(object[key])) {
+      return cb(null, keyDisplayName + ' must be a valid URL')
+    }
+
+    return cb(null, undefined)
+  }
+
+  return validate
+
+}
+
+/**
+* Validates that value is the correct form for a URL
+*
+* Uses the regex from Jquery Validation plugin
+*
+* @param {String} value URL to validate
+* @return {Boolean} True if value is a valid for a URL
+*/
+function urlMatch(value) {
+  /* jshint maxlen: false */
+  return (/^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)*(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i).test(value)
+}
